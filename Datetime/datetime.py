9bc4b699989f053803d53667b61bfc5372aa0f25
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2024-07-01 15:22:27 -0300

    Arquivos de Aulas

--- Datetime/datetime.py
+++ Datetime/datetime.py
@@ -1,11 +1,20 @@
-from datetime import date
+from datetime import date, datetime, time
 
 data = date(2005, 1, 19)
 print(data)
+print(date.today())
+
+
+data_hora = datetime(2005, 1, 19, 20, 40, 50)
+print(data_hora)
 
 
 
 import datetime 
 
-data = datetime.date(2005, 1, 19)
-print(data)
+dats = datetime.date(2005, 1, 19)
+print(dats)
+
+
+hora = time(20, 40, 0)
+print(hora)
